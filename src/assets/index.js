557cd5de7407290ce3bf9e57c60fefde197e5092
Afridi <afridi.hq97@gmail.com>
2024-12-20 19:34:39 +0530

    project update commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,6 @@ import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
-// import github from "./github.png";
 
 
 import menu from "./menu.svg";
@@ -22,7 +21,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
+import nextjs from "./tech/nextjs.png";
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
@@ -62,6 +61,7 @@ export {
   mongodb,
   nodejs,
   reactjs,
+  nextjs,
   redux,
   tailwind,
   typescript,
