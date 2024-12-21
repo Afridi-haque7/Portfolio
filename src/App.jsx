e74d6414bf6f54db6c7e21d17eabc1409cf18600
Afridi <afridi.hq97@gmail.com>
2024-12-21 14:05:07 +0530

    navbar fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useState,  useEffect } from "react";
+import { motion } from "framer-motion";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -10,6 +13,34 @@ import Skills from "./components/Skills";
 
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0
+  });
+  // console.log(mousePosition);
+  const [cursorVariant, setCursorVariant] = useState("default");
+  
+  useEffect (() => {
+    const mouseMove = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      });
+      
+    }
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    }
+  }, [])
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 1198,
+      y: mousePosition.y -10,
+    }
+  };
 
   return (
     <div className="relative z-0 bg-primary">
@@ -22,10 +53,15 @@ function App() {
       <Skills />
       <Projects />
       <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
+        <Contact />
+        <StarsCanvas />
       </div>
       <Footer />
+      {/* <motion.div
+        className="fixed z-20 top-0 right-0 bg-white w-6 h-6 rounded-full"
+        // variants={variants}
+        animate="default"
+      ></motion.div> */}
     </div>
   );
 }
